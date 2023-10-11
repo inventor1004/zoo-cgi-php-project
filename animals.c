448813330393b47ca839729706275ec6e064b7ca
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-10-10 21:35:58 -0400

    file opening system is updated
    it opens the txt file depending on which animal does user selects.

--- animals.c
+++ animals.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #pragma warning(disable : 4996)
 
@@ -22,14 +23,59 @@ int main()
 
   if (sscanf(data, "name=%99[^&]&animal=%99s", name, animal) == 2)
   {
-    printf("<h1 class ='greeting'>Hi %s. You choose %s Nice to meet you!</h1>", name, animal);
-    printf("<table>");
-    printf("<td>What kind of animal are you interested in?</td>");
-    printf("</table>");
+    // create the file pointer to get the animal discription txt file
+    FILE *ifp;
+    char line[100];
+
+    char filePath[50];
+    strcpy(filePath, "./theZoo/");
+
+    if (ifp == NULL)
+    {
+      printf("<h1 class ='greeting' style='color:red'>Can not open the file.</h1>");
+      return 1;
+    }
+    else
+    {
+      printf("<h1 class ='greeting'>Hi <u>%s</u>. You choose <u>%s</u>! Let me tell you about it!</h1>", name, animal);
+
+      if (strcmp(animal, "dragon") == 0)
+      {
+        strcat(filePath, "dragon.txt");
+      }
+      else if (strcmp(animal, "unicorn") == 0)
+      {
+        strcat(filePath, "unicorn.txt");
+      }
+      else if (strcmp(animal, "pegasus") == 0)
+      {
+        strcat(filePath, "pegasus.txt");
+      }
+      else if (strcmp(animal, "hydra") == 0)
+      {
+        strcat(filePath, "hydra.txt");
+      }
+      else if (strcmp(animal, "cerberus") == 0)
+      {
+        strcat(filePath, "cerberus.txt");
+      }
+      else if (strcmp(animal, "drunk-friend") == 0)
+      {
+        strcat(filePath, "drunk-friend.txt");
+      }
+
+      ifp = fopen(filePath, "r");
+
+      while (fgets(line, sizeof(line), ifp) != NULL)
+      {
+        printf("%s", line);
+      }
+      fclose(ifp);
+    }
   }
   else
   {
-    printf("<p>Invalid Data!</p>");
+    printf("<h1>Invalid Data!</h1>");
   }
 
   printf("</body></html>");
